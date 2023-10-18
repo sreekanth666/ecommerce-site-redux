d0d939af4ef43cfd62844b29254560db3c2cd10b
Sreekanth K <srekthk@gmail.com>
2023-10-18 12:59:26 +0530

    API integration and UI improvement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,30 @@ function Home() {
     return (
         <div className='container-fluid'>
             <Row>
-                <Col sm={12} md={6} lg={4} xl={3} className='m-3'>
-                    <Card style={{ width: '14rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body className='p-2'>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <div className='d-flex justify-content-between'>
-                                <Button variant="primary"><i class="fa-solid fa-heart"></i> Wishlist</Button>
-                                <Button variant="primary"><i class="fa-solid fa-cart-shopping"></i> Cart</Button>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                {
+                    data?.length > 0 ? data?.map((product, index) => (
+                        <Col className='mt-4' sm={12} md={6} lg={4} xl={3}>
+                            <Card style={{ width: '18rem'}}>
+                                <Card.Img height="200px" variant="top" src={product.thumbnail} />
+                                <Card.Body className='p-2'>
+                                    <Card.Title>{product.title}</Card.Title>
+                                    <Card.Text style={{fontSize:"small"}}>
+                                    {product.description.slice(0,60)}...
+                                    </Card.Text>
+                                    <Card.Text style={{fontSize:"small"}}>
+                                    <h5>${product.price}</h5>
+                                    </Card.Text>
+                                    <div className='d-flex justify-content-between'>
+                                        <Button variant="primary"><i class="fa-solid fa-heart"></i> Wishlist</Button>
+                                        <Button variant="primary"><i class="fa-solid fa-cart-shopping"></i> Cart</Button>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))
+                        : 
+                        <p className='text-center m-4'>Nothing to display</p>
+                }
             </Row>
         </div>
     )
